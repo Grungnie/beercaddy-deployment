edf44eb806ea1ee495a5b915b2611b40bf659976
Matthew Brown <mbrown1508@outlook.com>
2017-07-28 00:57:16 +1000

    Added thread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response, g
+from threading import Thread
 import os
 import subprocess
 import signal
@@ -13,36 +14,7 @@ def build():
     request_json = request.get_json()
 
     if 'repository' in request_json and 'url' in request_json['repository'] and request_json['repository']['url'] == GIT_URL:
-        # Kill current python process - how???
-        try:
-            os.killpg(os.getpgid(g.sleeping.pid), signal.SIGTERM)
-        except:
-            print('Looks like nothong was running')
-
-        # cd to root dir
-        os.chdir(PROGRAM_ROOT)
-
-        # blow away program
-        try:
-            subprocess.call('rm -R {}'.format(REPO_NAME), shell=True)
-        except:
-            print('No folder')
-
-        # clone repo
-        subprocess.call('git clone {}'.format(GIT_URL), shell=True)
-
-        # cd into dir
-        os.chdir('{}/{}'.format(PROGRAM_ROOT, REPO_NAME))
-
-        # create virtualenv
-        subprocess.call('virtualenv -p python3 venv', shell=True)
-        subprocess.call('source venv/bin/activate', shell=True)
-
-        # install requiremnets.txt
-        subprocess.call('pip install requirements.txt', shell=True)
-
-        # run program
-        g.sleeping = subprocess.Popen('python main.py', shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+        Thread(target=build_app).start()
     else:
         print('{} is not a valid git url'.format(request.get_json()['repository']['url']))
 
@@ -50,6 +22,39 @@ def build():
     resp = Response()
     resp.status_code = 202
     return resp
+
+
+def build_app():
+    # Kill current python process - how???
+    try:
+        os.killpg(os.getpgid(g.sleeping.pid), signal.SIGTERM)
+    except:
+        print('Looks like nothong was running')
+
+    # cd to root dir
+    os.chdir(PROGRAM_ROOT)
+
+    # blow away program
+    try:
+        subprocess.call('rm -R {}'.format(REPO_NAME), shell=True)
+    except:
+        print('No folder')
+
+    # clone repo
+    subprocess.call('git clone {}'.format(GIT_URL), shell=True)
+
+    # cd into dir
+    os.chdir('{}/{}'.format(PROGRAM_ROOT, REPO_NAME))
+
+    # create virtualenv
+    subprocess.call('virtualenv -p python3 venv', shell=True)
+    subprocess.call('source venv/bin/activate', shell=True)
+
+    # install requiremnets.txt
+    subprocess.call('pip install requirements.txt', shell=True)
+
+    # run program
+    g.sleeping = subprocess.Popen('python main.py', shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
 
 
 if __name__ == '__main__':

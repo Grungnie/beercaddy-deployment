55995b91b00bf3d687347c3b450f969176d82fc9
Matthew Brown <mbrown1508@outlook.com>
2017-08-04 01:06:07 +1000

    Added a save build folder option

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ virtualenv -p python3 $root/venv
 source $root/venv/bin/activate
 pip install -r $root/requirements.txt
 
-arduino --upload $root/arduino/arduino.ino --board arduino:avr:mega --port /dev/ttyACM0
+arduino --upload $root/arduino/arduino.ino --board arduino:avr:mega --port /dev/ttyACM0 --pref build.path=$root/../arduino-build
 
 exec python $root/main.py
